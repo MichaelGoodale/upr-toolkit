096b018cbdef2593befd52540d7f01190d319533
Michael Goodale <michael.goodale@mail.mcgill.ca>
2020-11-30 16:29:51 +0100

    made importing models optional

--- upr_toolkit/models/__init__.py
+++ upr_toolkit/models/__init__.py
@@ -1,8 +1,22 @@
 from .model import ModelData
-from .CPCData import CPCData
-from .VQWav2VecData import VQWav2VecData
-from .Wav2VecData import Wav2VecData, Wav2Vec2Data
-from .RandomData import RandomData
+
+#TODO: This is horrible, find a better way to optionally import
+try:
+    from .CPCData import CPCData
+except ImportError:
+    pass
+try:
+    from .VQWav2VecData import VQWav2VecData
+except ImportError:
+    pass
+try:
+    from .Wav2VecData import Wav2VecData, Wav2Vec2Data
+except ImportError:
+    pass
+try:
+    from .RandomData import RandomData
+except ImportError:
+    pass
 
 import logging
 logging.getLogger("fairseq").setLevel(logging.WARNING)
